1134f8f820d9209ebccf24f7111ef692d4c1fb48
subeeshpn <pn.subeesh@gmail.com>
2024-06-29 13:47:45 +0530

    added fileds to contact model object

--- ContactManagement.Api/Models/ContactModel.cs
+++ ContactManagement.Api/Models/ContactModel.cs
@@ -1,6 +1,16 @@
-﻿namespace ContactManagement.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagement.Api.Models
 {
     public class ContactModel
     {
+        [Required]
+        public int ContactId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailId { get; set; }
     }
 }
