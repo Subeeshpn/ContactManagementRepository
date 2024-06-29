f29115068ba94e694e146979084efe0b94f016e4
subeeshpn <pn.subeesh@gmail.com>
2024-06-29 13:43:19 +0530

    Added contact seed data to json file,added  read/ write functionalities from json file

--- ContactManagement.Api/WebApiHelpers/ContactJsonHelper.cs
+++ ContactManagement.Api/WebApiHelpers/ContactJsonHelper.cs
@@ -1,1 +1,26 @@
 ﻿
+using ContactManagement.Api.Models;
+using System.Text.Json;
+
+namespace ContactManagement.Api.WebApiHelpers
+{
+    public static class ContactJsonHelper
+    {
+        private static readonly string JsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "ContactDataJson.json");
+        public static IList<ContactModel> ReadFromJsonFile()
+        {
+            string jsonString = File.ReadAllText(JsonFilePath);
+            var contactlist = JsonSerializer.Deserialize<IList<ContactModel>>(jsonString);
+            return contactlist;
+        }
+
+
+
+        public static void WriteToJsonFile(IList<ContactModel> Contact)
+        {
+            string fileName = JsonFilePath;
+            string jsonString = JsonSerializer.Serialize(Contact);
+            File.WriteAllText(fileName, jsonString);
+        }
+    }
+}
